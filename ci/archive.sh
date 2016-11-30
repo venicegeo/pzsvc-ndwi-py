06a4f42fe0ee558f43487301cdde25eed0a31a89
bbarden <benjamin.m.barden@gmail.com>
2016-11-30 13:00:55 -0500

    First real attempt to get this running

---
+++ ci/archive.sh
@@ -0,0 +1,14 @@
+#! /bin/bash -ex
+
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
+popd > /dev/null
+
+export GOPATH=$root/gopath
+mkdir -p $GOPATH $GOPATH/bin $GOPATH/src $GOPATH/pkg
+source $root/ci/vars.sh
+
+go get -v github.com/venicegeo/pzsvc-exec/...
+
+src=$GOPATH/bin/pzsvc-exec
+mv $src $root/$APP.$EXT
