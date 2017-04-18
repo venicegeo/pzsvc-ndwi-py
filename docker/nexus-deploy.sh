4ddc2dbd35b199b83c2ca0b94f9ac4776af1a625
Ben Hosmer <ben.hosmer@gmail.com>
2017-04-18 14:32:22 -0400

    Update try new scheme for upload

--- docker/nexus-deploy.sh
+++ docker/nexus-deploy.sh
@@ -2,6 +2,6 @@
 
 # Helper shell script to upload the conda repo to nexus
 for f in $(find ./out/conda-bld -type f); do
-  curl -T "$f" -u $NEXUSUSER:$NEXUSPASS https://nexus.devops.geointservices.io/content/sites/beachfront-conda/$f
+  curl -T "$f" -u $NEXUSUSER:$NEXUSPASS https://nexus.devops.geointservices.io/content/repositories/beachfront-conda/$f
 done
 
