deffea94c0a2c5450468ffb395133f357d3f599e
Ben Hosmer <ben.hosmer@gmail.com>
2017-04-18 08:30:08 -0400

    Update cleaner conda build in docker container

--- docker/conda-build.sh
+++ docker/conda-build.sh
@@ -6,10 +6,13 @@ pushd `dirname $0`/pzsvc-ndwi-repo > /dev/null
 root=$(pwd -P)
 conda build agg
 conda config --add channels local
-conda build pypotrace
-conda build gippy
-conda build beachfront
-conda build bfalg-ndwi
+for i in pypotrace gippy beachfront bfalg-ndwi; do
+  conda build --no-anaconda-upload --quiet $i
+done
+#conda build pypotrace
+#conda build gippy
+#conda build beachfront
+#conda build bfalg-ndwi
 popd > /dev/null
 cp -r $HOME/miniconda2/conda-bld /$HOME/conda-repo
 
