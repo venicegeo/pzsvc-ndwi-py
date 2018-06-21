5335dc704b2cdd13e8a5b59f7971d3aa92fedc4f
wkhunt <wkhunt3@gmail.com>
2018-06-21 13:43:08 -0400

    Oneliner

--- tag.sh
+++ tag.sh
@@ -3,5 +3,4 @@ v=$1
 if [ -z "$v" ]; then
   exit 1
 fi
-sed -i "s/bfalg-ndwi=VERSION/bfalg-ndwi=$v/g" environment.yml
-sed -i "s/pzsvc-exec=VERSION/pzsvc-exec=$v/g" environment.yml
+sed -i "s/REPLACE_VERSION/$v/g" environment.yml
